8dd28b42bc60ddbdb9bb6ede828c74fbb88d6bd7
zhanjun <179896578@qq.com>
2017-07-13 11:15:26 +0800

    初始的demo提交

---
+++ restfulEasyDemo/src/main/java/restfulEasyDemo/testjava.java
@@ -0,0 +1,20 @@
+package restfulEasyDemo;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+@Path("test/")
+public class testjava {
+	
+	
+	
+	
+	@Path("go/{userName}")
+	@GET
+	public void testRestfulEasy(@PathParam("userName")String userName){
+		
+		System.out.println(userName);
+		
+	}
+	
+}
