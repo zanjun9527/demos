cadcfefd473b90c795f1d397f43fd2cae5e23c75
zanjun9527 <179896578@qq.com>
2020-06-15 10:37:53 +0800

    Create EchoClientHandler.java
    netty  client

---
+++ Netty_demo/EchoClientHandler.java
@@ -0,0 +1,36 @@
+package test.Netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+@ChannelHandler.Sharable
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+
+    protected void messageReceived(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) throws Exception {
+        System.out.println("Client received: "+byteBuf.toString(CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.copiedBuffer("netty client msg",CharsetUtil.UTF_8));
+    }
+
+    /**
+     * 异常输出
+     * @param ctx
+     * @param cause
+     * @throws Exception
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+
+}
